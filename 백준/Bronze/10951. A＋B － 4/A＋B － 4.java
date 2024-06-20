be5aade3b0acf985b9b3ea1135e22d987b105749
leessang96 <lgoda13@gmail.com>
2024-06-20 10:42:01 +0900

    [Bronze V] Title: A+B - 4, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		String str;
+		
+		while((str=br.readLine()) != null) {
+			st = new StringTokenizer(str, " ");
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append(a+b).append("\n");
+			
+			
+		}
+		
+		System.out.print(sb);
+		
+		
+		
+	}
+
+}
