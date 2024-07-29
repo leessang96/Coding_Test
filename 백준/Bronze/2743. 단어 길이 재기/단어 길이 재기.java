0f858bb99563c524212d4b23d3d3feee582b800f
SangYong Lee <lgoda13@gmail.com>
2024-07-29 22:10:33 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 208 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+    	
+    	Scanner sc = new Scanner(System.in);
+    
+    	String s = sc.nextLine();
+    	
+    	System.out.println(s.length());
+    	
+    	
+    	
+    }
+
+}
