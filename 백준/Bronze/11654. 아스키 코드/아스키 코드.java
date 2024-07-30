936e23c1b17b62739ad871a13458de16c580852f
SangYong Lee <lgoda13@gmail.com>
2024-07-30 23:21:40 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+    	
+    	Scanner sc = new Scanner(System.in);
+    
+    	int c = sc.next().charAt(0);
+    	
+    	System.out.println(c);
+    	
+    	
+    	
+    	
+    }
+
+}
