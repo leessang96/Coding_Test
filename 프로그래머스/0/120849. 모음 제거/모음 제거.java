4716787ac06933080ec024e65c52b57b709bed7a
SangYong Lee <lgoda13@gmail.com>
2024-08-01 11:33:10 +0900

    [level 0] Title: 모음 제거, Time: 2.02 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -2,20 +2,7 @@ class Solution {
     public String solution(String my_string) {
         String answer = "";
         
-        for(int i=0; i<my_string.length(); i++) {
-			
-			if(my_string.charAt(i) != 'a' && 
-			   my_string.charAt(i) != 'e' &&
-			   my_string.charAt(i) != 'i' &&
-			   my_string.charAt(i) != 'o' &&
-			   my_string.charAt(i) != 'u' ) {
-                
-				answer += my_string.charAt(i);
-                
-			}
-		}
-		
-		
+        answer += my_string.replaceAll("[aeiou]", "");
         
         return answer;
     }
