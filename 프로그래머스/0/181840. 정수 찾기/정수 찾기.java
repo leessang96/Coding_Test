4aeeb391948ed430011cb9f0061ad072e2673a80
SangYong Lee <lgoda13@gmail.com>
2024-10-07 23:20:02 +0900

    [level 0] Title: 정수 찾기, Time: 0.04 ms, Memory: 66.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] list, int n) {
+        int answer = 0;
+        
+        for(int i=0; i<list.length; i++){
+            if(list[i] == n){
+                answer = 1;
+            }else{
+                continue;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
