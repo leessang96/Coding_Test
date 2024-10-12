ced34cb4b40865009332fdfe341b5337c184bf76
SangYong Lee <lgoda13@gmail.com>
2024-10-12 23:38:36 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.32 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> solution(int n) {
+        
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        answer.add(n);
+
+        while(n != 1){
+            if(n % 2 == 0){
+                n /= 2;
+            }else{
+                n = 3 * n + 1;
+            }
+            
+            answer.add(n);
+        }
+        
+        return answer;
+    }
+}
