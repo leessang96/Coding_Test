5dfec098346899ea05539f2a3c07287b5a7d3dfb
SangYong Lee <lgoda13@gmail.com>
2024-10-27 12:06:48 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String s = String.valueOf(order);
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '3' || s.charAt(i) == '6' || s.charAt(i) == '9'){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
