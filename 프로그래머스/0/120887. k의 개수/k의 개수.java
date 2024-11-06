7fe0d5f61a5905a189c49e55c8e8420093f6f352
SangYong Lee <lgoda13@gmail.com>
2024-11-06 23:23:54 +0900

    [level 0] Title: k의 개수, Time: 3.80 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+
+        for(; i<=j; i++){
+            int n = i;
+            
+            while(n > 0){
+                if(n % 10 == k){
+                    answer++;
+                }
+                n /= 10;
+            }
+        }
+        
+        return answer;
+    }
+}
