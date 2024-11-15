0139b88d84f5d21dc54dd1977f2cca8a6e46ae79
SangYong Lee <lgoda13@gmail.com>
2024-11-15 23:16:42 +0900

    [level 0] Title: 글자 지우기, Time: 2.29 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        
+        String[] tmp = my_string.split("");
+        
+        for(int i=0; i<indices.length; i++){
+            tmp[indices[i]] = "";
+        }
+        
+        for(String s : tmp){
+            answer += s;            
+        }
+        
+        return answer;
+    }
+}
