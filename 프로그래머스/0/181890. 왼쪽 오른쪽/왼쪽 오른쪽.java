52b2b406125e645893d2e7cc269a13bfec4507b8
SangYong Lee <lgoda13@gmail.com>
2024-11-28 09:59:00 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -4,23 +4,26 @@ class Solution {
     public String[] solution(String[] str_list) {
         String[] answer = {};
         
-        int s = 0, e = 0;
-        for(int i=0; i<str_list.length; i++){
-            if(str_list[i].equals("l")){
-                e = i;
-                break;
-            }else if(str_list[i].equals("r")){
-                s = i + 1;
-                e = str_list.length;
-                break;
-            }
-        }
-        
-        answer = new String[e - s];
-        int index = 0;
-        for(int i=s; i<e; i++){
-            answer[index++] = str_list[i];
-        }
+       ArrayList<String> list = new ArrayList<String>();
+		
+		for(int i=0; i<str_list.length; i++) {
+			if(str_list[i].equals("l")) {
+				for(int j=0; j<i; j++) {
+					list.add(str_list[j]);
+				}
+				break;
+			}else if(str_list[i].equals("r")) {
+				for(int k=i+1; k<str_list.length; k++) {
+					list.add(str_list[k]);
+				}
+				break;
+			}
+		}
+
+		answer = new String[list.size()];
+		for(int i=0; i<answer.length; i++) {
+			answer[i] = list.get(i);
+		}
             
         return answer;
     }
