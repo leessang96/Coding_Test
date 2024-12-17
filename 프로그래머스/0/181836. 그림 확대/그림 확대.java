fa7cdb015d953f954ad507b5ef32082ec991e991
SangYong Lee <lgoda13@gmail.com>
2024-12-17 22:47:57 +0900

    [level 0] Title: 그림 확대, Time: 0.64 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length * k];
+        Arrays.fill(answer, "");
+        
+        int index = 0;
+        for(int i=0; i<picture.length; i++){
+            StringBuilder sb = new StringBuilder();
+            
+            for(int j=0; j<picture[i].length(); j++){
+                for(int l=0; l<k; l++){
+                    sb.append(picture[i].charAt(j));
+                }
+            }
+            
+            for(int j=0; j<k; j++){
+                answer[index++] = sb.toString();
+            }
+        }
+        return answer;
+    }
+}
