63e52b9967ac23afd89e5271d38fb24eba37c57b
SangYong Lee <lgoda13@gmail.com>
2025-02-02 12:04:40 +0900

    [level 1] Title: 콜라 문제, Time: 0.09 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while(n >= a){
+            if(n % a == 0){
+                answer += (n / a) * b;
+                n = (n / a) * b;
+            }else{
+                answer += (n / a) * b;
+                n = (n / a) * b + (n % a);
+            }
+        }
+        return answer;
+    }
+}
