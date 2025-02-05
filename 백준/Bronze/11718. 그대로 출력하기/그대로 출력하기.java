f00dcc069d6fb5396c5918bea74320ac4f6f4b2c
SangYong Lee <lgoda13@gmail.com>
2025-02-06 07:14:07 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 188 ms, Memory: 17920 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main{
+	
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()) {
+			String str = sc.nextLine();
+			
+			System.out.println(str);
+		}
+		sc.close();
+	}
+	
+	
+}
