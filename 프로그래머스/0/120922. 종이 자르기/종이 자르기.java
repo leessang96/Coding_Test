801aa27789373bcf70b082178b76d227dc60e911
SangYong Lee <lgoda13@gmail.com>
2025-12-06 11:24:02 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int M, int N) {
+        int answer = 0;
+        // 가로 
+        int x = M == 1 ? 0 : M - 1 ;
+        int y = N == 1 ? 0 : (N - 1) * M;
+        
+        answer = x + y;
+        return answer;
+    }
+}
