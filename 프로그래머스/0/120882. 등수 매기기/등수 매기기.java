f7bb3854e9a24a9cdf4dcf088c246c02ffb1dd32
SangYong Lee <lgoda13@gmail.com>
2025-12-15 10:54:16 +0900

    [level 0] Title: 등수 매기기, Time: 0.03 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer = new int[score.length];
+        int[] avg = new int[score.length];
+        
+        
+        for(int i=0; i<score.length; i++){
+            avg[i] = score[i][0] + score[i][1];
+        }
+        
+        for(int i=0; i<avg.length; i++){
+            int r = 1;
+            for(int j=0; j<avg.length; j++){
+                if(avg[i] < avg[j]){
+                    r++;
+                }
+            }
+            answer[i] = r;
+        }
+                
+        
+        return answer;
+    }
+}
